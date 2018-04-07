14783e48441964bc025b64fd5cf7d0699e6f5485
a198 <38160672+a198@users.noreply.github.com>
2018-04-07 13:39:51 -0500

    Add files via upload

---
+++ nombre.py
@@ -0,0 +1,26 @@
+"""
+nombre = "Octavio"
+apellido = "Resendiz"
+tonodepiel = "cafesito"
+colordeojos =  "caje oscuro"
+cervezapreferida =  "corona"
+barpreferido = "subte"
+bebidaalcoholicapreferida = "ron"
+print(nombre, "\n", apellido, "\n", tonodepiel, "\n", colordeojos, "\n", cervezapreferida, "\n", barpreferido, "\n", bebidaalcoholicapreferida, "\n")
+"""
+#Codigo 2
+nombre = input("nombre")
+apellido = input("apellido")
+tonodepiel = input("tonodepiel")
+colordeojos = input( "color de ojos")
+cervezapreferida = input( "cervaza preferida")
+barpreferido = input ("bar preferido")
+bebidaalcoholicapreferida = input("bebida alcoholica preferida")
+print("nombre",nombre, "\n",
+      "apellido", apellido, "\n",
+      "tonodepiel", tonodepiel, "\n",
+      "color de ojos", colordeojos,
+      "\n", "cervaza preferida",cervezapreferida,
+      "\n", "bar preferido",barpreferido,
+      "\n", "bebida alcoholica preferida", bebidaalcoholicapreferida, "\n")
+      
