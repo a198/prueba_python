c0a2659a913265241f25a1cbdc270c0d09fd6e8e
a198 <38160672+a198@users.noreply.github.com>
2018-04-14 13:47:56 -0500

    Add files via upload

---
+++ python/puntoinsert.py
@@ -0,0 +1,10 @@
+nombre=Andrea
+apellido=Caldera
+cuenta=314249612
+estatura=1.70
+variable=femenino
+lista=[nombre,apellido,cuenta,estatura,variable]
+print (lista)
+lista.insert(0,"andrea")
+print(lista)
+input("...")
