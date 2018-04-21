df86e6b15ea00c985a6b6aa55cd8782c2df25328
a198 <38160672+a198@users.noreply.github.com>
2018-04-21 10:55:18 -0500

    Add files via upload

---
+++ PythonA/mayorigualmenor.py
@@ -0,0 +1,11 @@
+a=int(input("Dame valor de a : "))
+b=int(input("Dame valor de b : "))
+
+if a>=b:
+  print("a es mayor que b")
+else:
+    print("a es menor que b")
+if a==b:
+  print("a es igual a b")
+print("Fin del programa")
+input(".............<3")
