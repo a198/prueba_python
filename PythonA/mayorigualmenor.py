29ec0f8a3d4637a13d25185dae6b18c6d5667945
a198 <38160672+a198@users.noreply.github.com>
2018-04-21 11:59:27 -0500

    Add files via upload

--- PythonA/mayorigualmenor.py
+++ PythonA/mayorigualmenor.py
@@ -8,4 +8,28 @@ else:
 if a==b:
   print("a es igual a b")
 print("Fin del programa")
-input(".............<3")
+input(""".............
+_______________________*…ooooooo…*
+_____________________*…oooo0oooooo…*
+____________________*…ooooooooooooo…*
+___________________*…ooooooooooooooo…*
+_____*…ooooo…*____*…ooooooooo….ooooo…*
+___*…oooooooo…*__*…oooooooooo………oooo…*
+_*…ooooooooooooooooooooooooooo……… oooo…*
+_*…ooooooooooooooooooooooooooo………ooooo...
+_*…oooooooooooooooooooooooooo………oooooo...
+_*…ooooooooooooooooooooooooo………oooooo…...
+__*…oooooooooooooooooooooooo……ooooooo…...
+___*…ooooooooooooooooooooooooooooooo…*
+____*…ooooooooooooooooooooooooooooo …*
+______*…ooooooooooooooooooooooooo…*
+________*…ooooooooooooooooooooo…*
+__________*…oooooooooooooooo…*
+____________*…oooooooooooo…*
+_____________*…ooooooooo…*
+_______________*…ooooo…*
+________________*…oooo…*
+_________________*…oo…*
+__________________*…*
+__________________**
+""")
